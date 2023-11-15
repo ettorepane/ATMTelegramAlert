8438f23dc8a61342499f8901f90fb4f83c0679bb
Ettore L. Pane <ettore.pane4@gmail.com>
2023-11-15 13:30:10 +0100

    Fixed a breaking bug

--- src/telegramManager/telegramBotSetter.js
+++ src/telegramManager/telegramBotSetter.js
@@ -1,11 +1,14 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.TELEGRAM_TOKEN;
+console.log("token: " + token);
 if (!token) {
     throw new Error('Missing TELEGRAM_TOKEN');
 }
 
-if (process.env.TELEGRAM_CHAT_ID) {
+const chatID = process.env.TELEGRAM_CHAT_ID;
+console.log("chatID: " + chatID);
+if (!chatID) {
     throw new Error('Missing TELEGRAM_CHAT_ID');
 }
 
